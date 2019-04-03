47d1d4ff879e00a1319bd0428d758ba7d4996204
ahmedsoud <ahmedsoud3@gmail.com>
2019-04-03 15:52:54 +0200

    fix post order: - fix orders not contain user data - handel guest add order

--- public/class-onyx-public.php
+++ public/class-onyx-public.php
@@ -246,16 +246,18 @@ class Onyx_Public {
     $orderTotal = $order->get_total();
 		$user = $order->get_user();
     $user_id = $order->get_user_id();
+
+    $userMobile = $order_data['billing']['phone'];
+    $userCode = $order_data['billing']['phone'];
     if ($user_id == 0) {
-        $user_id = 1;
+        $userCode = null;
     }
-		$userMobile = $order_data['shipping']['phone'];
-		$address_1 = $order_data['shipping']['address_1'];
-    $address_2 = $order_data['shipping']['address_2'];
-    $city = $order_data['shipping']['city'];
-    $state = $order_data['shipping']['state'];
-    $postcode = $order_data['shipping']['postcode'];
-    $country = $order_data['shipping']['country'];
+    $address_1 = $order_data['billing']['address_1'];
+    $address_2 = $order_data['billing']['address_2'];
+    $city = $order_data['billing']['city'];
+    $state = $order_data['billing']['state'];
+    $postcode = $order_data['billing']['postcode'];
+    $country = $order_data['billing']['country'];
 		$line_items = $order->get_items();
 		$orderItems =array();
     foreach ($line_items as $item_key => $item_values){
@@ -293,7 +295,7 @@ class Onyx_Public {
 		 $postOptions['values'] = array(
 			          'OrderNo'	   =>-1,
 								'OrderSer'   =>-1,
-								'Code'			 =>$userMobile,
+								'Code'			 =>$userCode,
 								'Name'			 =>$order_data['billing']['first_name'] .' '. $order_data['billing']['last_name'],
 								'CustomerType'=>1,
 								'FiscalYear'   => $apiSettings['accounting_year'],
